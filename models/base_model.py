a56fe5e9174edc4417e6610238495941e9ef58ed
cloobTech <belkid98@gmail.com>
2023-03-21 14:09:49 -0700

    staging

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 import uuid
 from datetime import datetime
 
+
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -13,7 +17,6 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
@@ -31,6 +34,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):

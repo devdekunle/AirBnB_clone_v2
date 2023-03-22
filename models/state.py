25221797952d11758e3ab03a162b021955c3d81b
cloobTech <belkid98@gmail.com>
2023-03-22 09:23:47 -0700

    solved

--- models/state.py
+++ models/state.py
@@ -12,7 +12,7 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state', cascade='delete-orphan')
+    cities = relationship('City', back_populates='state', cascade='delete, delete-orphan')
 
     @property
     def cities(self):

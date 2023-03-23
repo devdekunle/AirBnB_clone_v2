c46ee57ae4c5166d3f3223122127a1b94552a785
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-03-23 12:37:51 +0100

    fixed Place class errors

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,7 +27,7 @@ class DBStorage:
     __session = None
     classes = {'User': User,
            'Place': Place, 'State': State, 'City': City,
-           'Amenity': Amenity, 'Review': Review}
+           'Review': Review}
 
 
     def __init__(self):

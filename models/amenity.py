de57c61ab6b055d5a8c47c35d04938f505297bdd
cloobTech <belkid98@gmail.com>
2023-03-25 02:46:35 -0700

    Revert "fixing amenity"
    This reverts commit 5773fbd56368a5e4e9ae6cdb8438c4c40e3d5b30.
    
    Trying to revert to <fixing Amenity>

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey, Integer
-from sqlalchemy.orm import relationship
-from models.place import Place
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    place_amenities = relationship('Place', secondary=place_amenity, backref='amenity')
+class Amenity(BaseModel):
+    name = ""

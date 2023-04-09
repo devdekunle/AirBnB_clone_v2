82457b167b098a996669ca96486e6a23624818ac
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-10 00:25:44 +0100

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,6 +9,16 @@ import os
 do_pack = __import__('1-pack_web_static').do_pack
 do_deploy = __import__('2-do_deploy_web_static').do_deploy
 
+
+env.hosts = ["ubuntu@100.24.236.208", "ubuntu@54.146.75.104"]
+env.user = 'ubuntu'
+env.key_file = "~/.ssh/id_rsa"
+
 def deploy():
-""" This function fully deploys the web_static to the web servers"""
+    """ This function fully deploys the web_static to the web servers"""
+    archive = do_pack()
+    if archive:
+        return do_deploy(archive)
+    else:
+        return False
 

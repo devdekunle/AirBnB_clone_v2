3bafad6e66a5fd59623967b0ecd074f7c7eb9d9f
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-09 09:40:47 +0100

    archive web_static files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,23 +5,12 @@ of the web_static folder of your AirBnB Clone repo,
 using the function do_pack.
 """
 import os
-from fabric.api import local
+from fabric.api import local, run_once
 from datetime import datetime
 
-
+@run_once
 def do_pack():
-    """
-    archive the web_static folder using the format described
-    Prototype: def do_pack():
-    All files in the folder web_static must be added to the final archive
-    All archives must be stored in the folder versions
-    (your function should create this folder if it doesn’t exist)
-    The name of the archive created must be
-    web_static_<year><month><day><hour><minute><second>.tgz
-    The function do_pack must return the archive path
-    if the archive has been correctly generated. Otherwise,
-    it should return None
-    """
+    """Archive the web_static folder using the format described """
     if not os.path.isdir("versions"):
         os.mkdir("versions")
     time = datetime.now()

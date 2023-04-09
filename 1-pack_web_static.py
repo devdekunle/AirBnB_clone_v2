d80615224ed6eeb8cace4e009cd5a3a281d2268b
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-09 13:18:37 +0100

    generate archive for web_statics

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """A Fabric script that generates a .tgz archive from the contents
 of the web_static folder of the AirBnB Clone repo,
-using the function do_pack."""
-
+using the function do_pack.
+"""
 import os
 from fabric.api import local
 from datetime import datetime

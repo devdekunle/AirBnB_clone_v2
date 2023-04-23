375bd2251022a45878f0de3e21fe87f63d062efd
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-23 18:22:00 +0100

    fix amenity class

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -2,6 +2,6 @@
 -- create a new user and grant privileges
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd1_Adekunle9102';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';

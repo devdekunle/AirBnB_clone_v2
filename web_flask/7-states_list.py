66257e82af882a2fddd5fae45fb97424e208256d
Abraham Samuel Adekunle <abrahamadekunle50@gmail.com>
2023-04-26 23:57:40 +0100

    list states in database using flask app and rendering html page with list of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -26,9 +26,27 @@ echo "quit" | HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd
 HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db
 HBNB_TYPE_STORAGE=db ./console.py
 """
-from flask import Flask
+from models import storage
+from flask import Flask, render_template
+from models.state import State
+
+
 app = Flask(__name__)
 app.url_map.strict_slashes=False
+states = storage.all(State)
+
+@app.teardown_appcontext
+def tear_down(exc):
+    storage.close()
+
+@app.route('/states_list')
+def states_list():
+    """ list of all states."""
+    all_states = list(storage.all(State).values())
+    all_states.sort(key=lambda x: x.name)
+    ctxt = {'states': all_states}
+    return render_template('7-states_list.html', **ctxt)
 
 
-
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
